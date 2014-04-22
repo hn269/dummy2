e8ddb23c5ad40ee93a07cd0d85e0cf08019d3bb5
hn269 <hn269@cornell.edu>
2014-04-22 16:57:49 -0400

    first sync

---
+++ README.txt
@@ -0,0 +1,38 @@
+==========================================
+Python Documentation and Releasing Project
+==========================================
+
+
+Introduction
+------------
+
+This project is an attempt at documenting Python code and prepare a ready-to-
+release Python package, including proper tests and documentations. The ultimate 
+goal of this project is to release a Python package of John A. Marohn 
+(jam99@cornell.edu) onto GitHub and PyPI.
+
+:Authors: Hoang Long Nguyen
+:Email: hn269@cornell.edu
+
+The dummy2 program
+------------------
+
+This program was developed in the course of this project. 
+This is a simple dummy text editor.
+
+Links:
+------
+
+ReadTheDocs: https://readthedocs.org/projects/dummy2/
+
+Quickstart:
+-----------
+
++ Run dummy2.exe
++ Edit text, open and save files
+
+Dependencies:
+-------------
+
+jinja 2.7.2 for documentation.
+
---
+++ setup.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python
+
+from __future__ import print_function
+from distutils.core  import setup
+
+import io
+import os
+import sys
+import py2exe
+import dummy2
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+long_description = read('README.txt','requirements.txt','changes.txt',)
+
+setup(
+    name='dummy2',
+    version=dummy2.__version__,
+    url='http://github.com/hn269/dummy2/',
+    license='Cornell University',
+    author='Hoang Long Nguyen',
+    author_email='hn269@cornell.com',
+    description='Dummy package adding, subtracting or multiplying two numbers - to try releasing a Python package',
+    long_description=long_description,
+    packages=['dummy2',],
+    data_files=[('',['requirements.txt','changes.txt','icons\Dummy_icon.ico']),],
+    windows=[
+        {
+        "script":'dummy2\dummy2.0.py',
+        "icon_resources": [(0,"Dummy_icon.ico")],
+        }],
+    console=[{
+        "script":'dummy2\dummy2.0.py',
+        "icon_resources": [(0,"Dummy_icon.ico")],
+        }],
+    #zip_safe = True,
+)
